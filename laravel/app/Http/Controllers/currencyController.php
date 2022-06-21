4e11f2f7818a32899f1ea2fb54e976ad4a108c70
AishaBukar <muhammedaisha84@yahoo.com>
2022-06-21 14:16:47 +0100

    changes to webpack, currencyController,index and app files

---
+++ laravel/app/Http/Controllers/currencyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use AmrShawky\LaravelCurrency\Facade\Currency;
+
+class currencyController extends Controller
+{
+    public function index(){
+        return view('index',[
+            'codes' => Currency::rates()->latest()->get()
+        ]);
+    }
+    public function convert(){
+
+        $currency= Currency::convert()->from('USD')->to('EUR')->amount(100)->get();
+        
+        
+        // dd($codes);
+
+
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\currencyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [currencyController::class, 'index']);
+
+Route::post('/convert', [currencyController::class, 'convert']);
